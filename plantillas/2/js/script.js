d1b7ae4af420625f1155de333ee630c7400d4bd4
Sun.tea <santi.vallejo@gmail.com>
2020-02-07 03:07:43 -0300

    actualizacion sitio web plantilla n2

---
+++ plantillas/2/js/script.js
@@ -0,0 +1,30 @@
+  $(document).ready(function () {
+
+    const sections = {
+        header_id : $('#header').offset().top,
+        obras_id : $('#main-obras').offset().top,
+        proyectos_id : $('#main-proyectos').offset().top,
+        aboutme_id : $('#main-about-me').offset().top,
+        contact_id : $('#main-contact').offset().top
+    }
+    const {obras_id, proyectos_id, aboutme_id, contact_id} = sections;
+    let counter = 0;
+    let timeout;
+
+
+    $('html, body').on('wheel', function(event){  
+        clearTimeout(timeout);   
+        timeout = setTimeout(function() {
+        // deltaY obviously records vertical scroll, deltaX and deltaZ exist too
+    if(event.originalEvent.deltaY > 0){
+        counter >= 0 && counter <= 3 ? counter++ : counter == 4;
+        }
+    else {
+        counter >= 1 ? counter-- : counter = 0;
+     }
+     $('html, body').animate({
+        scrollTop: Object.entries(sections)[counter][1]
+    }, 400);
+    }, 50);
+  });
+  });
---
+++ plantillas/2/slide-show/README.markdown
@@ -0,0 +1,4 @@
+# Split Slick Slideshow
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/supah/pen/zZaPeE](https://codepen.io/supah/pen/zZaPeE).
+
+ Vertical slideshow in split screen
