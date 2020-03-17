b6daee317d25f176af9357d05a7b2287fbea7352
Sun.tea <santi.vallejo@gmail.com>
2020-03-17 19:57:46 -0300

    added soundmix

---
+++ new/templates/2/js/ipad-support.js
@@ -0,0 +1,3 @@
+const alto = window.innerHeight;
+document.querySelector('.header').style.height = alto + 'px';
+document.querySelector('.header-background').style.height = alto + 'px';
---
+++ new/templates/sound-mixer/sonido.js
@@ -0,0 +1,76 @@
+const arraySounds = [];
+const soundsPlaying = [];
+
+const INITIAL_VOLUME =  0.8 ;
+const NUM_SOUNDS = 9;
+
+const container = document.getElementById('container');
+
+
+for (let i = 0; i < NUM_SOUNDS; i++) {
+  //howl sound
+  arraySounds[i] = new Howl({
+    src: [`./sounds/${i}.wav`],
+    loop: true,
+    volume: generateRandomVolumeNumber()
+  });
+  //create array of buttons
+  const soundButton = document.createElement("button");
+  soundButton.innerHTML = `[ Paused ]`;
+
+  //create sliders
+  const slider = document.createElement("input");
+  passSlideAttributes(slider, i);
+
+  const slideContainer = document.createElement('div')
+  slideContainer.classList.add('slide-container');
+  slideContainer.append(slider);
+
+  const soundContainer = document.createElement('div');
+  soundContainer.classList.add('sound-container');
+  soundContainer.append(soundButton,slideContainer);
+  container.append(soundContainer);
+
+  //add play stop
+  soundButton.addEventListener("click", function() {
+    clickButtonHandler(i, arraySounds[i], this);
+  });
+
+  //add volume
+  slider.addEventListener("input", e => {
+    const volValue = e.target.value;
+    arraySounds[i].volume(volValue);
+    // soundsPlaying[idx].volume = volValue;
+  })
+}
+
+function clickButtonHandler(idx, sound, thisBtn) {
+  const soundVol = arraySounds[idx]._volume;
+  if (!soundsPlaying[idx] || soundsPlaying[idx].status === "Paused") {
+    soundsPlaying[idx] = {};
+    const id = sound.play();
+    soundsPlaying[idx].id = id;
+    sound.fade(0, soundVol, 1000, soundsPlaying[idx].id);
+    soundsPlaying[idx].status = "Playing";
+  } else  {
+    sound.fade(soundVol, 0, 1000, soundsPlaying[idx].id);
+    setTimeout(() => sound.pause(soundsPlaying[idx].id), 1000);
+    soundsPlaying[idx].status = "Paused";
+  }
+  thisBtn.classList.toggle('playing');
+  thisBtn.innerHTML = `[ ${soundsPlaying[idx].status} ] `;
+}
+
+function passSlideAttributes(slider,idx) {
+  slider.setAttribute("type", "range");
+  slider.setAttribute("max", "1");
+  slider.setAttribute("min", "0");
+  slider.setAttribute("step", "0.05");
+  slider.setAttribute("name", "single-volume");
+  slider.setAttribute("class", "slider");
+  slider.value = arraySounds[idx]._volume;
+}
+
+function generateRandomVolumeNumber () {
+  return (Math.random() * 80 + 30) / 100;
+}
